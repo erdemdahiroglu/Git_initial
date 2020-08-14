eb23a4f151afc12047fa6283ddf569734917b6fd
erdemdahiroglu <erdemdahiroglu@gmail.com>
2020-08-14 16:00:08 +0300

    calculations_1 added

--- hello.py
+++ hello.py
@@ -5,11 +5,18 @@ def adding(s1, s2):
     return s1 + s2
 
 
+def calculation_1(s1, s2):
+    return (s1 * s2) - (s1 + s2)
+
+
 def main():
     n1 = random.randint(5, 9)
     n2 = random.randint(4, 7)
     n3 = adding(n1, n2)
     print(n1, n2, n3)
+
+    n4 = calculation_1(n1, n2)
+    print(n1, n2, n4)
 
 
 main()
