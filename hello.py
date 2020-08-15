81e7fd369c85de63ec8cdd9b6c735c0f8bcb4317
erdemdahiroglu <69199250+erdemdahiroglu@users.noreply.github.com>
2020-08-16 01:35:43 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,10 +1,6 @@
 import random
 
 
-def adding(s1, s2):
-    return s1 + s2
-
-
 def calculation_1(s1, s2):
     return (s1 * s2) - (s1 + s2)
 
@@ -17,8 +13,6 @@ def main():
     print("Version 1")
     n1 = random.randint(5, 9)
     n2 = random.randint(4, 7)
-    n3 = adding(n1, n2)
-    print(n1, n2, n3)
 
     n4 = calculation_1(n1, n2)
     print(n1, n2, n4)
